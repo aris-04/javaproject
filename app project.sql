52faaca24fe7c277a7efd414b26acbfc893a6cf7
vedita-20 <veditamaini10@gmail.com>
2025-10-02 22:03:06 +0530

    Add files via upload

---
+++ app project.sql
@@ -0,0 +1,28 @@
+create database student
+use student
+
+CREATE TABLE Login (
+    username VARCHAR(50),
+    password VARCHAR(50),
+    email VARCHAR(100)
+);
+
+
+CREATE TABLE StudentDetails (
+    name VARCHAR(100),
+    student_id INT,
+    course VARCHAR(100)
+);
+
+
+CREATE TABLE Grade (
+    subject_name VARCHAR(100),
+    grade VARCHAR(5)
+);
+select*from Login
+
+
+
+
+
+
